d2c9a920d90f4f50df1cab134656ac0831b93ab0
Andriy Zakharko <azakharko@windowslive.com>
2018-07-27 20:01:04 -0500

    Add project files.

---
+++ SignalTransformation.Core/Tester.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SignalTransformation.Core
+{
+
+    public static class Tester
+    {
+        public static string Test(double[] array, double[] correct, double a, int s)
+        {
+            const double Threshold = 1E-15;
+            var calculated = Transformer.Transform(array, a, s);
+
+            // this approach does not work out of the box because even if each and every
+            // element of Sequence the same, whole Sequence is different, so I have two choices:
+            // * implement DoubleComparer class and use it in Enumerable.SequenceEqual(calculated, correct, new DoubleComparer())
+            // * or use manual one-by-one sequence comparison (simple and easy, but not the best option)
+            //return Enumerable.SequenceEqual(calculated, correct) ? "test pass" : "test fail";
+
+            bool result = true;
+            for (int i = 0; i < correct.Length; i++)
+            {
+                if (Math.Abs(correct[i] - calculated[i]) > Threshold)
+                {
+                    result = false;
+                }
+            }
+            return result ? "test pass" : "test fail";
+        }
+
+        public static double[] GenerateArray(int amplitude, int size)
+        {
+            double[] result = new double[size];
+            var rnd = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = rnd.Next(0, amplitude);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SignalTransformation.Core/Transformer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SignalTransformation.Core
+{
+    public static class Transformer
+    {
+        public static double[] Transform(double[] given_array, double new_amplitude, int new_size)
+        {
+            double[] new_array = new double[new_size];
+            double[] middle = new double[given_array.Length * new_size];
+            double given_array_max = 0;
+
+            for (int i = 0; i < given_array.Length; i++)
+            {
+                if (given_array[i] > given_array_max)
+                {
+                    given_array_max = given_array[i];
+                }
+            }
+
+            if (given_array_max == 0)
+                throw new ArgumentException("Given input array must contains at least one element greater than zero");
+
+            // building new intermediate buffer (array)
+            // this algorythm is not optimal from memory consumption point of view
+            // but it gives the best approximation
+            int index = 0;
+            foreach (var item in given_array)
+            {
+                for (int i = 0; i < new_size; i++)
+                {
+                    middle[index * new_size + i] = item;
+                }
+                index++;
+            }
+
+            index = 0;
+            for (int i = 0; i < new_size; i++)
+            {
+                double calc_v = 0;
+
+                for (int j = 0; j < given_array.Length; j++)
+                {
+                    calc_v = calc_v + middle[index * given_array.Length + j];
+                }
+                index++;
+
+                new_array[i] = (new_amplitude / given_array_max) * (calc_v / given_array.Length);
+            }
+            
+            return new_array;
+        }
+    }
+}
